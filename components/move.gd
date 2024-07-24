b16e133931ad5074b1a2335bc188cb5cec4ade5c
Bruno Mello <bruno.barros.mello@gmail.com>
2024-07-23 21:15:06 -0300

    move, move_input, move_stats and move_clamp components

--- components/move.gd
+++ components/move.gd
@@ -1,12 +1,9 @@
 class_name MoveComponent
 extends Node
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass  # Replace with function body.
+@export var actor: Node2D
+@export var velocity: Vector2
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _process(delta: float) -> void:
+	actor.translate(velocity * delta)
