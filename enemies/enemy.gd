04bc805317c10fe3f121a761653d5ffdb0d1fb75
Bruno Mello <bruno.barros.mello@gmail.com>
2024-07-24 18:43:48 -0300

    hitboxes and hurtboxes

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -6,8 +6,12 @@ extends Node2D
 @onready var scale_component: ScaleComponent = $ScaleComponent
 @onready var flash_component: FlashComponent = $FlashComponent
 @onready var shake_component: ShakeComponent = $ShakeComponent
+@onready var hitbox_component: HitboxComponent = $HitboxComponent
+@onready var hurtbox_component: HurtboxComponent = $HurtboxComponent
 
 
 func _ready():
 	visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
-
+	hurtbox_component.hurt.connect(func(_hitbox: HitboxComponent):
+		queue_free()
+	)
