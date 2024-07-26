5b39707f63ce02209ffc5c531a1e6b532e159417
Bruno Mello <bruno.barros.mello@gmail.com>
2024-07-25 21:00:30 -0300

    yellow enemy

--- EnemyGenerator.gd
+++ EnemyGenerator.gd
@@ -1,16 +1,20 @@
 extends Node2D
 
 @export var GreenEnemyScene: PackedScene
+@export var YellowEnemyScene: PackedScene
 
 var margin = 8
 var screen_width = ProjectSettings.get_setting("display/window/size/viewport_width")
 
 @onready var spawner_component: SpawnerComponent = $SpawnerComponent
+
 @onready var green_enemy_spawn_timer: Timer = $GreenEnemySpawnTimer
+@onready var yellow_enemy_spawn_timer: Timer = $YellowEnemySpawnTimer
 
 
 func _ready():
 	green_enemy_spawn_timer.timeout.connect(handle_spawn.bind(GreenEnemyScene, green_enemy_spawn_timer))
+	yellow_enemy_spawn_timer.timeout.connect(handle_spawn.bind(YellowEnemyScene, yellow_enemy_spawn_timer))
 
 
 func  handle_spawn(enemy_scene: PackedScene, timer: Timer) -> void:
